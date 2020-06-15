2b579197b58fb94b40f9693e1e6c4cb4152f0d33
pranitlahiri <65769328+pranitlahiri@users.noreply.github.com>
2020-06-15 16:05:33 +0530

    Add files via upload

--- ball.js
+++ ball.js
@@ -6,12 +6,14 @@ class Ball{
             friction:0.5,
             density:1.2
         }
-        this.body.rectangle(200,200,50,50,options)
+        this.body = Bodies.rectangle(100,350,50,50,options);
         this.width = 50;
         this.height = 50;
         World.add(world, this.body);
     }
     display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
         push();
         translate(pos.x, pos.y);
         rotate(angle);
--- sketch.js
+++ sketch.js
@@ -6,16 +6,26 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload(){}
-
+ 
 function setup() {
 	createCanvas(800, 700);
 
 
 	engine = Engine.create();
-	world = engine.world;
+  world = engine.world;
+
+  basketBottom=createSprite(650,height-50,200,20);
+	basketBottom.shapeColor=color(168,32,190);
+
+	basketLeft=createSprite(750,height-90,20,100);
+	basketLeft.shapeColor=color(168,32,190);
+
+	basketRight=createSprite(550,height-90,20,100);
+  basketRight.shapeColor=color(168,32,190);
 
 	Engine.run(engine);
-	ground = new Ground (width/2,height-35,width,20);
+  ground = new Ground (width/2,height-35,width,20);
+  ball = new Ball (width/4,height-50,20,20);
   
 }
 
@@ -23,11 +33,14 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background("maroon");
-  Engine.update(Engine);
   ground.display();
-  
+  ball.display();
   drawSprites();
- 
+}
+function KeyPressed(){
+  if(KeyCode === 32) {
+    Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
+  }
 }
 
 
